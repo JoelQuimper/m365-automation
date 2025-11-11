6591eafd0e6d0cc9922f231fe3b6962b29383917
Joel Quimper <dieu@jquimperdev.onmicrosoft.com>
2025-11-11 13:09:59 -0500

    Réorganiser les sections de staging, commit et push dans git-cheat.ps1 pour une meilleure clarté

--- git-cheat.ps1
+++ git-cheat.ps1
@@ -1,17 +1,22 @@
 $branch = ""
 
 # Create a new branch
-git branch $branch
-git checkout $branch
+git branch $branch #create new branch
+git checkout $branch #switch to new branch
 
-# Stage/Commit/Push
+# Stage/Commit
 $commitMessage = ""
-git add .
-git commit -m $commitMessage
-git push --set-upstream origin $branch
+git add . #stage all changes
+git commit -m $commitMessage #commit staged changes with message
+
+# Push
+git push --set-upstream origin $branch #the first time
+git push #subsequent times
+
+# Create a Pull Request
 
 # Cleanup
-git checkout main
-git fetch --prune
-git pull
-git branch -D $branch
+git checkout main #switch back to main branch
+git fetch --prune #update remote tracking branches to remove origin branches that have been deleted
+git pull #update local main branch with the merge Pull Request changes
+git branch -D $branch #delete local branch
